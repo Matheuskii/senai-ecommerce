c6d51f8ca461f82092c7b389b957ca731fafa29b
Matheus <alvesdemelomatheus4@gmail.com>
2026-04-23 11:11:44 -0300

    feat: add class Endereco with business rules

---
+++ api-ecommerce/src/main/java/br/com/senai/api_ecommerce/cliente/DadosAtualizarCliente.java
@@ -0,0 +1,23 @@
+package br.com.senai.api_ecommerce.cliente;
+
+import br.com.senai.api_ecommerce.endereco.DadosAtualizarEndereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+public record DadosAtualizarCliente(
+        Long id,
+
+        @Size(min=3, max=100)
+        String nome,
+
+        @Email
+        String email,
+
+        @Size(max=20)
+        String telefone,
+
+        @Valid
+        DadosAtualizarEndereco endereco
+) {
+}
---
+++ api-ecommerce/src/main/java/br/com/senai/api_ecommerce/endereco/DadosAtualizarEndereco.java
@@ -0,0 +1,26 @@
+package br.com.senai.api_ecommerce.endereco;
+
+import jakarta.validation.constraints.Pattern;
+
+public record DadosAtualizarEndereco(
+
+            String logradouro,
+
+            String bairro,
+
+            String cidade,
+
+            @Pattern(regexp = "\\d{8}") //"\\d" = só números, "{8}" = precisa ter 8 números"
+            String cep,
+
+            @Pattern(regexp = "[A-Z]{2}")
+            String uf,
+
+            String numero,
+
+            String complemento
+
+    ) {
+    }
+
+
